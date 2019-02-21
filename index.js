782137eb2aaecfdea70341dfc91b0539a38f142c
Mohammed Noori <kmanoorib@gmail.com>
2019-02-21 02:38:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,27 +3,19 @@
 customerName = "Bob";
 
 function upperCaseCustomerName(name) {
-  return 
+  return name.toUpperCase();
 }
 
-upperCaseCustomerName()
-modifies the customerName variable ‣
-AssertionError: expected undefined to equal 'bob'
-    at Context.<anonymous> (test/indexTest.js:12:38)expect(window.customerName).to.equal('bob');
-upperCaseCustomerName();
-expect(window.customerName).to.equal('BOB');
-setBestCustomer()
-setBestCustomer ‣
-ReferenceError: setBestCustomer is not defined
-    at Context.<anonymous> (test/indexTest.js:23:7)expect(window.bestCustomer).to.equal(undefined);
-setBestCustomer();
-expect(window.bestCustomer).to.equal('not bob');
-overwriteBestCustomer()
-overwrites the best customer ‣
-ReferenceError: overwriteBestCustomer is not defined
-    at Context.<anonymous> (test/indexTest.js:31:7)overwriteBestCustomer('maybe bob');
-expect(window.bestCustomer).to.equal('maybe bob');
-changeLeastFavoriteCustomer()
-unsuccessfully tries to reassign the least favorite customer ‣
-ReferenceError: changeLeastFavoriteCustomer is not defined
-    at Context.<anonymous> (test/indexTest.js:39:14)expect(changeLeastFavoriteCustomer).to.throw(TypeError);
+function setBestCustomer() {
+  return bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  return bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "someone";
+
+function changeLeastFavoriteCustomer() {
+  return leastFavoriteCustomer = "no one";
+}
